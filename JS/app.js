9efffb377ae224c54cae6ac39a38d152ee0ddacc
david <david.hanke@student.hs-rm.de>
2018-11-12 12:01:25 +0100

    neue Dateien

---
+++ JS/app.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+/* Ihr jQuery Code */ 
+$(function() {
+  var menuVisible = false;
+  $('#icon').click(function() {
+    if (menuVisible) {
+      $('#menu').css({'display':'none'});
+      menuVisible = false;
+      return;
+    }
+    $('#menu').css({'display':'block'});
+    menuVisible = true;
+  });
+});
+
+$(window) .resize(function(){
+    $('body, html') .css ('overflow', 'hidden');
+    var screenWidth = $(window).width();
+    $('body, html') .css ('overflow', 'visible');
+    
+        if (screenWidth >= 769) {
+            $('#menu').css ({'display':'block'});
+        } else {
+            $('#menu').css ({'display':'none'});
+        }
+    
+});
+    
+$(function() {
+      $('a').click(function() {
+if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+&& location.hostname == this.hostname) {
+    var $target = $(this.hash);
+    $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+    if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body').animate({scrollTop: targetOffset}, 700);
+        return false;
+    }
+}
+  });
+});
+    
+    
+    
+})
